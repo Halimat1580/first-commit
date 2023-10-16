f1e2193a3d87899901438ee0f7405ca4e87f75d3
Halimat1580 <halimausman43@yahoo.com>
2023-10-16 10:27:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+'use strict' 
+ let random =  Math.ceil(Math.random() * 6) ; 
+ console.log(random); 
+ let compGuess = document.querySelector('.comp-guess') 
+ const userGuess =document.querySelector('.user-guess') 
+ let trials = document.querySelector('.trials') 
+ const btn = document.querySelector('.check') 
+ const start = document.querySelector('.start') 
+ const btn1 =  document.querySelector('.again') 
+ const body = document.querySelector('body') 
+  
+ const end = function(trials){ 
+     if(trials.textContent>1){ 
+         trials.textContent-- 
+  
+     }else if(trials.textContent==1){ 
+  
+         window.location.reload() 
+  
+     } 
+  
+ } 
+  
+  
+ 
+  
+  btn.addEventListener('click',function(){ 
+     if(+userGuess.value === random){ 
+  
+         document.body.style.backgroundColor='green' 
+  
+         start.textContent= 'Correct answer🎉🎉' 
+         compGuess.textContent = random 
+  
+     }else if (+userGuess.value > random){ 
+         document.body.style.backgroundColor='orange' 
+         start.textContent = 'Too high,try again' 
+         userGuess.value=' ' 
+         end(trials) 
+  
+  
+     }else if (+userGuess.value < random){ 
+         document.body.style.backgroundColor='blue' 
+         start.textContent = 'Too low' 
+         userGuess.value=' ' 
+         end(trials) 
+  
+  
+     } 
+  
+  
+ }) 
+ btn1.addEventListener('click',()=>{ 
+     window.location.reload() 
+ })
